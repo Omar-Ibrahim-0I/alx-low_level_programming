39ea28b111f714853fd1814a3acb7a0a03b6b7de
omar <omar.shams.eldin.edu@gmail.com>
2023-02-17 11:01:45 -0800

    0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
- *
  * Description: 'the program's description'
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char m[] = "\"Programming is like building a multilingual puzzle";
+	char m[] = "\"Programming is like building a multilingual puzzle \n";	
 	puts(m);
 	return (0);
 }
