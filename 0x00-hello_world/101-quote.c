8470e15c91f5ced4e25d9bf3e9609f3618ecd8dc
omar <omar.shams.eldin.edu@gmail.com>
2023-02-18 14:09:23 -0800

    1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *s = "and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n";
+	while (*s) putchar(*s++);
+	return (1);
+}
+
