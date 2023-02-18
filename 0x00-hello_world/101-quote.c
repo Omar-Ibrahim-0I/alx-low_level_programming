a61300bfacf8d8533e196ea3359d4367d5bcd3d4
omar <omar.shams.eldin.edu@gmail.com>
2023-02-18 14:13:11 -0800

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- * Description: 'the program's description'
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char *s = "and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n";
-	while (*s) putchar(*s++);
-	return (1);
+int main(void){
+  char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  while (*s) putchar(*s++);
 }
-
