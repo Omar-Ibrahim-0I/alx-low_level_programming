0fef2ea20f4bd0ec9d5bebb2517efd17cd722b4d
omar <omar.shams.eldin.edu@gmail.com>
2023-02-20 13:11:03 -0800

    1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* 
+ * betty style doc for function main goes there
+ * main function start point
+*/
+int main(void)
+{
+	char* alphabet = "abcdfghijklmnoprstuvwxyz\n";
+
+	while (*alphabet != '\0') {
+   		putchar(*alphabet);
+		alphabet++;
+	}
+	return (0);
+}
