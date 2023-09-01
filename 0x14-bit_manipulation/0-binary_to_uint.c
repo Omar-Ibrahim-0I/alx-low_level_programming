8c88d5c32cd34c7c9cdb8fd2a757a2360f910c6a
omar shams <omar.shams.eldin.edu@gmail.com>
2023-09-01 12:12:19 -0700

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+
+int _strlen(const char *str);
+int _pwr(int base, int raise);
+
+/**
+ * binary_to_uint - convert binary to decimal
+ * @b: binary
+ *
+ * Return: decimal or 0 if typing error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, j = _strlen(b) - 1, total = 0;
+
+	while  (i <= j)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		total += _pwr(2, j - i) * (b[i] - '0');
+		i++;
+	}
+
+	return (total);
+}
+
+/**
+ * _strlen  - count the length of string
+ * @str: memory address of string
+ *
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _pwr  - exponent
+ * @base: base number
+ * @raise: exponent number
+ *
+ * Return: length
+ */
+int _pwr(int base, int raise)
+{
+	int total = base;
+
+	if (raise == 0)
+		return (1);
+	else if (raise == 1)
+		return (base);
+	while (raise > 1)
+	{
+		total *= base;
+		raise--;
+	}
+	return (total);
+}
---
+++ 0x14-bit_manipulation/test.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", binary_to_uint("101"));
+	return 0;
+}
